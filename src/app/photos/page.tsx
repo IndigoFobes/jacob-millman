b9d73fd77687fd13a397077c9a5e7eefb8a9a17c
Indigo <indigo.crandell@gmail.com>
2023-10-12 17:08:43 -0400

    Setting up file structure.

---
+++ src/app/photos/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>Photos page</h1>
+    )
+}
---
+++ src/app/resume/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>Resume page</h1>
+    )
+}
